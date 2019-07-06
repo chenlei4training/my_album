9e6776db74dba24a3abe20e1f213810bdfdf2264
unknown <you@example.com>
2019-07-06 21:34:39 +0800

    init

---
+++ src/views/home/coms/mainMenu.vue
@@ -0,0 +1,27 @@
+<template>
+    
+</template>
+
+
+<script> 
+export default {
+    name:"@@@ give me a name,驼峰命名",
+    data(){
+        //如果这个组件的data由vueX管理，不需要这个方法data()
+        return {}
+    },
+    created(){ //hook 钩子函数
+
+    },
+
+    components:{
+
+    }
+}
+</script>
+
+
+<style lang="scss" scope>
+//代码模板，仅用于代码 copy & paste
+</style>
+
---
+++ src/views/home/index.vue
@@ -0,0 +1,26 @@
+<template>
+    
+</template>
+
+
+<script> 
+export default {
+    name:"@@@ give me a name,驼峰命名",
+    data(){
+        //如果这个组件的data由vueX管理，不需要这个方法data()
+        return {}
+    },
+    created(){ //hook 钩子函数
+
+    },
+
+    components:{
+
+    }
+}
+</script>
+
+
+<style lang="scss" scope>
+
+</style>
